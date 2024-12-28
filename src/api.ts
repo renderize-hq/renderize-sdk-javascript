60d57673fc5ada81558abcba089b6802a7ca2ee1
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2024-12-28 23:27:16 +0100

    feat(http-client): added source header in http client (#1)

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,11 @@
 import { ofetch } from 'ofetch';
+import { version } from '../package.json';
 
 export function createApiClient({ apiKey, baseApiUrl }: { apiKey: string; baseApiUrl: string }) {
   const apiClient = ofetch.create({
     headers: {
-      Authorization: `Bearer ${apiKey}`,
+      'Authorization': `Bearer ${apiKey}`,
+      'X-Renderize-Source': `renderize-sdk-javascript/${version}`,
     },
     baseURL: baseApiUrl,
   });
